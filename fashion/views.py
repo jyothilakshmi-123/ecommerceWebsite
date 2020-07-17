3c22b229a00e2ff04e40b4247000b07abd3697e1
Jyothilakshmi C <jyothilakshmi1491@gmail.com>
2020-07-17 14:54:44 +0530

    template added, give all path and it works

--- fashion/views.py
+++ fashion/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("<h1>Hello World</h1>")
+def index(request):
+    
+    return render(request,'index.html')
+   
