e8b611f1de5bc92fc2fc568b7e5436bbf8c0b5e5
Jyothilakshmi C <jyothilakshmi1491@gmail.com>
2020-07-20 20:01:54 +0530

    first page dyanamic content migrate into database

--- fashion/models.py
+++ fashion/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Specialday(models.Model):
+   
+    catagory = models.CharField(max_length=100)
+    day_name = models.CharField(max_length=100)
+    desc = models.TextField()
+    img  = models.ImageField(upload_to='pics')
+    sale = models.BooleanField(default = False)
--- fashion/views.py
+++ fashion/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import Specialday
 
 # Create your views here.
 def index(request):
+    spcl1 = Specialday()
+    spcl1.catagory = 'Women'
+    spcl1.day_name = 'Year End Sale'
+    spcl1.desc ='Now started year end sale with flat 50%'
+    spcl1.img = 'hero-1.jpg'
+    spcl1.sale = True
     
-    return render(request,'index.html')
+    spcl2 = Specialday()
+    spcl2.catagory = 'Kids'
+    spcl2.day_name = 'Year End Sale'
+    spcl2.desc ='Now started year end sale with flat 50%'
+    spcl2.img = 'hero-2.jpg'
+    spcl2.sale = True
+
+    spcl3 = Specialday()
+    spcl3.catagory = 'Men'
+    spcl3.day_name = 'Year End Sale'
+    spcl3.desc ='Now started year end sale with flat 50%'
+    spcl3.img = 'hero-2.jpg'
+    spcl3.sale = False
+    
+    spcl = [spcl1,spcl2,spcl3]
+    
+    
+    return render(request,'index.html',{'spcl':spcl})
    
