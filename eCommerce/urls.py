1fad8e4d5f509c1e5026906c8497ebd436946465
Jyothilakshmi C <jyothilakshmi1491@gmail.com>
2020-07-31 12:09:26 +0530

    shop page added

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include('fashion.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.urls'))
+    path('accounts/',include('accounts.urls')),
+    path('products/',include('products.urls'))
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
