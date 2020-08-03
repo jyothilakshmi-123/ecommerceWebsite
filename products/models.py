a32c9b427b7962379a7afd1412b1e3ed1033ad6e
Jyothilakshmi C <jyothilakshmi1491@gmail.com>
2020-08-03 14:46:05 +0530

    sale page complete

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+    
+    image = models.ImageField(upload_to = 'pics')
+    name =  models.CharField(max_length=100)
+    desc = models.TextField()
+    sale = models.BooleanField(default=False)
+    price1 = models.FloatField()
+    price2 = models.FloatField()
--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import product
 
 # Create your views here.
 def shop(request):
-    return render(request,'shop.html')
+    prods = product.objects.all()
+
+    return render(request,'shop.html',{'prods':prods})
